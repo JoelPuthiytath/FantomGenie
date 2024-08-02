40ad74f1b08954d9b8391ecaf89a8b7931689f81
joel <joelbabyputhiyath@gmail.com>
2024-08-02 12:47:45 +0530

    Project is ready to go

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
